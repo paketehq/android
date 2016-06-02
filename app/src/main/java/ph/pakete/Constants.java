98c0bd9eac210d1441c1eb83c736172371f2640f
Royce Dy <rady182@me.com>
2016-06-02 18:17:39 +0800

    added Constants

---
+++ app/src/main/java/ph/pakete/Constants.java
@@ -0,0 +1,9 @@
+package ph.pakete;
+
+public interface Constants {
+    public interface SharedPreferences {
+        public static final String NAME = "ph.pakete.preferences";
+        public static final String SORT_BY_KEY = "SORT_BY_KEY";
+        public static final String GROUP_BY_DELIVERED_KEY = "GROUP_BY_DELIVERED_KEY";
+    }
+}
